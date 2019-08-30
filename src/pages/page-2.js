6faa7dd1f494a8dc41dbdc7124063629464d7c0f
Madeline Miller <mnmiller1@me.com>
2019-08-30 10:27:13 +1000

    Added starter content for workshop

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,23 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+import styled from 'styled-components';
+
+const FancyH1 = styled.h1`
+  color: purple;
+  text-transform: uppercase;
+`;
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <FancyH1>Hi from the second page</FancyH1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
-)
+);
 
-export default SecondPage
+export default SecondPage;
